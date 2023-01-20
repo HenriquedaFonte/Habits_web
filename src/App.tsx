2b3316c7cbed8cf1e152cfc5bbb8acbd75c6a19f
Henrique da Fonte <henriquegdafonte@gmail.com>
2023-01-19 19:41:31 -0500

    Feat: Create Header, HabitDay and SummaryTable

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-function App() {
+import './styles/global.css';
+// import { Habit } from "./components/Habit";
+
+import { Header } from './components/Header';
+import { SummaryTable } from './components/SummaryTable';
+
+export function App() {
 
   return (
-    <h1>Teste</h1>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-20'>
+        <Header/>
+        <SummaryTable/>
+      </div>
+    </div>
   )
 }
 
-export default App
+
