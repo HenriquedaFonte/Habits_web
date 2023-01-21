55ef8fa55c96f7b13bab5f7a62f4e3e02b87ab6f
Henrique da Fonte <henriquegdafonte@gmail.com>
2023-01-20 21:31:20 -0500

    Feat: Create components HabitBay, Header, NewHabitForm, ProgressBar and SummaryTable

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -27,7 +27,11 @@ export function SummaryTable(){
         
         {summaryDates.map(date =>{
           return (
-            <HabitDay key={date.toString()}/>
+            <HabitDay 
+              amount={5} 
+              completed={Math.round(Math.random() * 5)} 
+              key={date.toString()}
+            />
           )
         })}
 
