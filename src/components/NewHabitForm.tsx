650427b5a970a12acd82cd306ba51f3cdf07493e
Henrique da Fonte <henriquegdafonte@gmail.com>
2023-01-25 10:23:25 -0500

    Feat: all funcionability an Styles

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -9,7 +9,7 @@ const availableWeekDays = [
   'Tuesday', 
   'Wednesday', 
   'Thursday',
-  'friday', 
+  'Friday', 
   'Saturday'
 ]
 
@@ -48,21 +48,21 @@ export function NewHabitForm() {
   }
 
   return (
-    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
-      <label htmlFor="title" className="font-semibold leading-tight">
+    <form onSubmit={createNewHabit} className='w-full flex flex-col mt-6'>
+      <label htmlFor='title' className='font-semibold leading-tight'>
         What is your habit?
       </label>
       <input
-        type="text"
-        id="title"
-        placeholder="ex.: Exercise, drink water, etc..."
-        className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+        type='text'
+        id='title'
+        placeholder='ex.: Exercise, drink water, etc...'
+        className='p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-violet-600 focus:ring-offset-2 focus:ring-offset-zinc-900'
         autoFocus
         value={title}
         onChange={event => setTitle(event.target.value)}
       />
 
-      <label htmlFor="" className="font-semibold leading-tight mt-4">
+      <label htmlFor='' className='font-semibold leading-tight mt-4'>
         What is the recurrence?
       </label>
 
@@ -73,20 +73,20 @@ export function NewHabitForm() {
               
               <Checkbox.Root 
                 key={weekDay} 
-                className="flex items-center gap-3 group"
+                className='flex items-center gap-3 group focus:outline-none'
                 checked={weekDays.includes(index)}
                 onCheckedChange={() => {handleToggleWeekDays(index)}}
               >
           
-              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
                 
                 <Checkbox.Indicator>
-                  <Check size={20} className="text-white " />
+                  <Check size={20} className='text-white ' />
                 </Checkbox.Indicator>
     
               </div>
     
-              <span className="text-white leading-tight ">
+              <span className='text-white leading-tight '>
                 {weekDay}
               </span>
     
@@ -99,10 +99,10 @@ export function NewHabitForm() {
       </div>
 
       <button
-        type="submit"
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+        type='submit'
+        className='mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900'
       >
-        <Check size={20} weight="bold" />
+        <Check size={20} weight='bold' />
         Confirm
       </button>
     </form>
